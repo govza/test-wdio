55f6002accdf5d280c99e4b5cb7699e7a8de479d
Rasul Abu Muhammad Amin <abuhasub@gmail.com>
2024-10-06 01:48:52 +0200

    tests

--- test/specs/downloads.e2e.ts
+++ test/specs/downloads.e2e.ts
@@ -1,7 +1,13 @@
 describe("Downloads", () => {
-  it("downloads page should work", async () => {
+  it("h1 should have text", async () => {
     await browser.url('chrome://downloads//')
     const toolbar = await browser.$('h1').getElement();
     await expect(toolbar).toHaveText('Download history');
+  });
+
+  it("downloads-toolbar should be existing", async () => {
+    await browser.url('chrome://downloads//')
+    const toolbar = await browser.$('downloads-toolbar').getElement();
+    await expect(toolbar).toBeExisting();
   });
 });
--- test/specs/extension.e2e.ts
+++ test/specs/extension.e2e.ts
@@ -1,14 +1,14 @@
 describe("Example", () => {
+  it("toolbar should be existing", async () => {
+    await browser.url('chrome://extensions/')
+
+    const toolbar = await browser.$('h1').getElement();
+    await expect(toolbar).toHaveText('Extensions');
+  });
+
   it("extensions page should work", async () => {
     await browser.url('chrome://extensions/')
     const extensions = await browser.$$('extensions-item').getElements();
     await expect(extensions.length).toBeGreaterThan(0);
-  });
-
-  it("toolbar should be existing", async () => {
-    await browser.url('chrome://extensions/')
-
-    const toolbar = await browser.$('#toolbar').getElement();
-    await expect(toolbar).toBeExisting();
   });
 });
